20431fd281483c8dbd35f8f2ddaffcd0a3140c69
zhanggaojie <zhanggaojie@che.com>
2016-05-20 18:09:20 +0800

    Signed-off-by: zhanggaojie <zhanggaojie@che.com>

---
+++ src/main/java/com/zhang/module/login/controller/LoginController.java
@@ -0,0 +1,26 @@
+/**
+ * 文件名：com.zhang.module.login.controller.LoginController.java<br/>
+ * 创建时间：2016年5月20日 下午4:51:54<br/>
+ * 创建者：zhanggaojie<br/>
+ * 修改者：暂无<br/>
+ * 修改简述：暂无<br/>
+ * 修改详述：
+ * <p>
+ * 暂无<br/>
+ * </p>
+ * 修改时间：暂无<br/>
+ */
+package com.zhang.module.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	//TODO 记得要写注释，方便别人，成就自己。
+	@RequestMapping(value="/login.do")
+	public String loginPage(){
+		return "login";
+	}
+}
+
